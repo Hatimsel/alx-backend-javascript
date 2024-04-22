26bdb2c2ed11e296bf488668adf8415493fdf07d
Hatim Selmun <selmun13@gmail.com>
2024-04-22 18:11:39 +0100

    iterating through report objects

---
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -0,0 +1,15 @@
+export default function createIteratorObject(report) {
+  function* iterateEmployees() {
+	for (const department in report.allEmployees) {
+	  for (const employee of report.allEmployees[department]) {
+		yield employee;
+	  }
+	}
+  }
+
+  const iterator = {
+	[Symbol.iterator]: iterateEmployees
+  };
+
+  return iterator;
+}
