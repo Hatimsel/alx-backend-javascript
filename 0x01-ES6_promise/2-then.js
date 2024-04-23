879c999a292c4191f82943fa3568043f1fdc69a9
Hatim Selmun <selmun13@gmail.com>
2024-04-23 20:30:06 +0100

    handle the response

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,12 +1,19 @@
 export default function handleResponseFromAPI(promise) {
   const resolvedObj = { status: 200, body: 'success' };
+  const error = new Error();
   return new Promise((resolve, reject) => {
     if (promise) {
       resolve(resolvedObj);
-      console.log('Got a response from the API');
     } else {
-      reject(new Error());
-      console.log('Got a response from the API');
+      reject(error);
     }
   });
 }
+
+handleResponseFromAPI()
+  .then(() => {
+    console.log('Got a reponse from the API');
+  })
+  .catch(() => {
+    console.log('Got a response from the API');
+  });
