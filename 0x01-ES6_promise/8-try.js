ea3ded721a9056027ea30ef7c5d5d79991acabed
Hatim Selmun <selmun13@gmail.com>
2024-04-24 20:36:47 +0100

    try catch

---
+++ 0x01-ES6_promise/8-try.js
@@ -0,0 +1,10 @@
+export default function divideFunction(numerator, denominator) {
+  try {
+    if (denominator !== 0) {
+      return numerator / denominator;
+    }
+    throw new Error('cannot divide by 0');
+  } catch (error) {
+    return (error);
+  }
+}
