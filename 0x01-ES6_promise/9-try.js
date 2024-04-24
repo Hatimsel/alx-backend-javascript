01c66fa8826c73e2a3eb2530589c6bc7860e9e98
Hatim Selmun <selmun13@gmail.com>
2024-04-24 21:00:17 +0100

    catch the error message

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -5,7 +5,7 @@ export default function guardrail(mathFunction) {
     queue.push(result, 'Guardrail was processed');
     return queue;
   } catch (error) {
-    queue.push(error.message, 'Guardrail was processed');
+    queue.push(error.toString(), 'Guardrail was processed');
     return queue;
   }
 }
