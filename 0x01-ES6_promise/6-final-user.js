a0793cc3f1db025c5dc039c93875f572ea2b6c58
Hatim Selmun <selmun13@gmail.com>
2024-04-24 19:26:45 +0100

    handle multiple promises

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,28 +2,20 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default async function handleProfileSignup(firstName, lastName, fileName) {
-  try {
-    const promiseOne = await signUpUser(firstName, lastName);
-    const promiseTwo = await uploadPhoto(fileName);
-    const result = [];
-    promiseOne.then((data) => (promiseOne.state === 'fulfilled' ? result.push({
-      status: promiseOne.state,
-      value: data,
-    }) : result.push({
-      status: promiseOne.state,
-      value: new Error(''),
-    })));
-    promiseTwo.then((data) => (promiseTwo.state === 'fulfilled' ? result.push({
-      status: promiseTwo.state,
-      value: data,
-    }) : result.push({
-      status: promiseTwo.state,
-      value: new Error(''),
-    })));
+  const [userResult, photoResult] = await Promise.allSettled([
+    signUpUser(firstName, lastName),
+    uploadPhoto(fileName),
+  ]);
 
-    return result;
-  } catch (error) {
-    // console.log('An error occurred', error);
-    return ['rejected', 'rejected'];
-  }
+  const handleSettledPromise = (result) => {
+    if (result.status === 'fulfilled') {
+      return result.value;
+    }
+    return {
+      status: result.status,
+      value: `Error: ${result.reason.message}`,
+    };
+  };
+
+  return [handleSettledPromise(userResult), handleSettledPromise(photoResult)];
 }
