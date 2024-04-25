698fd12ba15d7015beaea09a3d2f0a137ed9dd1e
Hatim Selmun <selmun13@gmail.com>
2024-04-25 13:03:45 +0100

    getter and setter

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -10,7 +10,12 @@ export default class HolbertonCourse {
   }
 
   set name(name) {
-    this._name = typeof name === 'string' ? name : (() => { throw new TypeError(); });
+    // this._name = typeof name === 'string' ? name : (() => { throw new TypeError(); });
+    if (typeof name === 'string') {
+      this._name = name;
+    } else {
+      throw new TypeError('Name must be a string');
+    }
   }
 
   get length() {
@@ -18,7 +23,12 @@ export default class HolbertonCourse {
   }
 
   set length(length) {
-    this._length = typeof length === 'number' ? length : (() => { throw new TypeError(); });
+    // this._length = typeof length === 'number' ? length : (() => { throw new TypeError(); });
+    if (typeof length === 'number') {
+      this._length = length;
+    } else {
+      throw new TypeError('Length must be a number');
+    }
   }
 
   get students() {
@@ -26,6 +36,12 @@ export default class HolbertonCourse {
   }
 
   set students(students) {
-    this._students = students instanceof Array && typeof students[0] === 'string' ? students : (() => { throw new TypeError(); });
+    if (students instanceof Array) {
+      if (students.every((student) => typeof student === 'string')) {
+        this._students = students;
+      }
+    } else {
+      throw new TypeError('Students must be an array of strings');
+    }
   }
 }
