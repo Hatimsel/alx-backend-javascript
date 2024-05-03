1cd30418d256c4721c6b8411359538fa2c4de2c8
Hatim Selmun <selmun13@gmail.com>
2024-05-03 15:56:30 +0100

    Fixing typed array

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,12 +1,13 @@
 export default function createInt8TypedArray(length, position, value) {
-  if (position < 0 || position >= length) {
-    throw new Error('Position outside range');
-  }
-
   const buffer = new ArrayBuffer(length);
 
-  const typedArr = new Int8Array(buffer);
-  typedArr[position] = value;
+  const typedArr = new DataView(buffer, 0, length);
 
-  return buffer;
+  try {
+    typedArr.setInt8(position, value);
+  } catch (error) {
+    console.log('Position outside range', error);
+  }
+
+  return typedArr;
 }
