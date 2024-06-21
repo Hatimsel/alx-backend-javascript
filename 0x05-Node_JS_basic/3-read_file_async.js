f82b90b841351e760373158346c44a96ca775886
Hatim Selmun <selmun13@gmail.com>
2024-06-21 19:40:38 +0100

    countstudents asynchronously

---
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -0,0 +1,41 @@
+const fs = require('fs').promises;
+
+function countStudents(path) {
+  return fs.readFile(path, 'utf8')
+    .then((data) => {
+      const lines = data.trim().split('\n');
+      const fields = {};
+
+      for (const line of lines.slice(1)) { // Skip the header line
+        const parts = line.split(',');
+        if (parts.length >= 4) {
+          const field = parts[3].trim();
+          if (field) {
+            if (!fields[field]) {
+              fields[field] = [];
+            }
+            const firstName = parts[0].trim();
+            if (firstName) {
+              fields[field].push(firstName);
+            }
+          }
+        }
+      }
+
+      const totalStudents = Object.values(fields).reduce((acc, curr) => acc + curr.length, 0);
+      console.log(`Number of students: ${totalStudents}`);
+
+      for (const field in fields) {
+        if (Object.prototype.hasOwnProperty.call(fields, field)) {
+          const listOfStudents = fields[field].join(', ');
+          console.log(`Number of students in ${field}: ${fields[field].length}. List: ${listOfStudents}`);
+        }
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      throw new Error('Cannot load the database');
+    });
+}
+
+module.exports = countStudents;
