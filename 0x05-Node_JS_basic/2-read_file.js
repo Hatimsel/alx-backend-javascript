373fd41687fe0b50ed88d58f6655d594fd64a9e5
Hatim Selmun <selmun13@gmail.com>
2024-06-22 13:43:23 +0100

    raed database synchronously

---
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -0,0 +1,31 @@
+const fs = require('node:fs');
+
+function countStudents(path) {
+  try {
+    const content = fs.readFileSync(path, 'utf8');
+    const lines = content.split('\n');
+    lines.shift();
+
+    console.log(`Number of students: ${(lines.length)}`);
+    const fields = {};
+
+    for (const line of lines) {
+      const components = line.split(',');
+      if (!(components[3] in fields)) {
+        fields[components[3]] = [];
+      }
+      fields[components[3]].push(components[0]);
+    }
+    for (const field in fields) {
+      if (Object.prototype.hasOwnProperty.call(fields, field)) {
+        const students = fields[field];
+        console.log(`Number of students in ${field}: `
+            + `${students.length}. List: ${students.join(', ')}`);
+      }
+    }
+  } catch (err) {
+    console.log('Cannot load the database');
+  }
+}
+
+countStudents('./database.csv');
